fdb8a3023cb4bb4f451c81c90ae253b635a8611f
Ravi <ravikumr0403@gmail.com>
2022-04-18 16:53:18 +0530

    chore: apollo client setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { gql, useQuery } from "@apollo/client";
 const App = () => {
+  const FORM_QUERY = gql`
+    query MyQuery {
+      quizzr_quizzes {
+        questions_aggregate {
+          aggregate {
+            count
+          }
+        }
+      }
+    }
+  `;
+  const { loading, data, error } = useQuery(FORM_QUERY);
+  console.log(loading, data, error?.graphQLErrors, error?.networkError);
   return <div>Hello</div>;
 };
 
---
+++ src/config/apollo-config.js
@@ -0,0 +1,21 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_HASURA_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export { client };
